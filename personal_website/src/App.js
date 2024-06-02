f55a19a7bc6cbead2b33d6fe2d209c6f313073ae
bkty1122 <kwoktszyi1122@gmail.com>
2024-06-02 22:56:43 +0800

    upload study materials

--- personal_website/src/App.js
+++ personal_website/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Simple React Counter</p>
+        <div>
+          <button onClick={() => setCount(count - 1)}>-</button>
+          <span style={{ margin: '0 10px' }}>{count}</span>
+          <button onClick={() => setCount(count + 1)}>+</button>
+        </div>
       </header>
     </div>
   );
